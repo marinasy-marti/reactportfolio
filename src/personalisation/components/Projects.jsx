fa75d61e28c5e2c377436c17e6c8a44f75aafda1
Marinasy Behamby <behambymarinasy@gmail.com>
2022-12-01 09:52:18 +0300

    done with project component

--- src/personalisation/components/Projects.jsx
+++ src/personalisation/components/Projects.jsx
@@ -2,7 +2,46 @@ import '../styles/Projects.css'
 
 const Projects = () => {
 
-    return <>Projects</>
+    return <>
+    
+        <div className='d-flex  flex-column'>
+            <div className='education-one my-2  justify-content-between d-flex flex-row'>
+                <div className='d-flex flex-column new-one'>
+                    <span className='university-name'>Portfolio</span>
+                    <span className='university-degree'>Technologies: React, NodeJS, FontAwesome, Bootstrap 5, Lottie Files</span>
+                    <span > <a className='poc' href={"http://github.com/marinasy-marti/reactportfolio"}>Project Link</a></span>
+                </div>
+                <div>
+                    <span className='year'>Nov 2022</span>
+                </div>
+            </div>
+        </div>
+        <div className='d-flex  flex-column'>
+            <div className='education-one my-2  justify-content-between d-flex flex-row'>
+                <div className='d-flex flex-column new-one'>
+                    <span className='university-name'>TimeClock for employees</span>
+                    <span className='university-degree'>Technologies: React, C++, Arduino , MySQL</span>
+                    <span><a className='poc' href="http://github.com/marinasy-marti/systemeDePointageParEmpreinteDigital">Project Link</a></span>
+                </div>
+                <div>
+                    <span className='year'>June - Dec 2022</span>
+                </div>
+            </div>
+        </div>
+
+        <div className='d-flex  flex-column'>
+            <div className='education-one my-2  justify-content-between d-flex flex-row'>
+                <div className='d-flex flex-column new-one'>
+                    <span className='university-name'>Mikrotik Hotspot</span>
+                    <span className='university-degree'>Technologies: RouterOS Mirktorik, HTML, CSS</span>
+                </div>
+                <div>
+                    <span className='year'>Nov 2022</span>
+                </div>
+            </div>
+        </div>
+
+    </>
 }
 
 
