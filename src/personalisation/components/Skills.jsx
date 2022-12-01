cc81102cc49c39c2663cef82adedced478fd45c2
Marinasy Behamby <behambymarinasy@gmail.com>
2022-12-01 09:32:42 +0300

    done with skills component

--- src/personalisation/components/Skills.jsx
+++ src/personalisation/components/Skills.jsx
@@ -2,7 +2,181 @@ import '../styles/Skills.css'
 
 const Skills = () => {
 
-    return <>Skills</>
+    const value_dev = [
+        {
+            text:"Front End: React JS, Vue JS",
+            percentage: 50,
+        },
+        {
+            text:"Bootstrap 4, 5",
+            percentage:40,
+        },
+        {
+            text:"Redux, VueX",
+            percentage:30.
+        },
+        {
+            text:"Backend: Flask, Node JS, C++",
+            percentage: 30,
+        },
+       
+        {
+            text:"Flask",
+            percentage:40,
+        },
+        {
+            text:"SQL",
+            percentage: 70,
+        },
+        {
+            text:"C and C++",
+            percentage:80,
+        },
+       
+        {
+            text:"React Native",
+            percentage:20,
+        },
+        {
+            text:"Versioning: Git",
+            percentage:60,
+        },
+    ]
+
+    const value_net = [
+        {
+            text:"Docker",
+            percentage: 50,
+        },
+        {
+            text:"CI/CD",
+            percentage: 10,
+        },
+        {
+            text:"Shell Scripting",
+            percentage:60,
+        },
+        {
+            text:"Linux administration ",
+            percentage:80,
+        },
+        {
+            text:"iptables",
+            percentage:40,
+        },
+        {
+            text:"pfSense",
+            percentage:50,
+        },
+        {
+            text:"Cisco",
+            percentage: 30,
+        },
+        {
+            text:"Mikrotik  ",
+            percentage:45,
+        },
+        {
+            text:"Network Programming: C++, Java",
+            percentage:50,
+        },
+        
+    ]
+
+    const value_other = [
+        {
+            text:"Arduino",
+            percentage: 85,
+        },
+        {
+            text:"Embeded System",
+            percentage: 60,
+        },
+        {
+            text:"Mircocontroller Programming",
+            percentage:60,
+        },
+        {
+            text:"Robotics",
+            percentage:60,
+        },        
+    ]
+    return <>
+        <div className='skills-container'>
+            <div className='marker'>
+                <div className='box-title'>
+                    <span>Development</span>    
+                </div>
+                <div className='box-content'>
+                    <div className='row skills-box'>
+                        {
+                            value_dev.map((value_dev,index) => {
+                                return <>
+                                    <div className='col-lg-4 col-mg-12 col-sm-12 my-1'>
+                                        <span className='language'>{value_dev.text}</span>
+                                        <div className='progress-some'>
+                                            <div className='progress-new' style={{width: `${value_dev.percentage}%`}}></div>
+                                        </div>
+                                    </div>
+                                </>
+                            })
+                        } 
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className='skills-container'>
+            <div className='marker'>
+                <div className='box-title'>
+                    <span>Networking and System Admninistration</span>    
+                </div>
+                <div className='box-content'>
+                    <div className='row skills-box'>
+                        {
+                            value_net.map((value_net,index) => {
+                                return <>
+                                    <div className='col-lg-4 col-mg-12 col-sm-12 my-1'>
+                                        <span className='language'>{value_net.text}</span>
+                                        <div className='progress-some'>
+                                            <div className='progress-new' style={{width: `${value_net.percentage}%`}}></div>
+                                        </div>
+                                    </div>
+                                </>
+                            })
+                        } 
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className='skills-container'>
+            <div className='marker'>
+                <div className='box-title'>
+                    <span>Other skills</span>    
+                </div>
+                <div className='box-content'>
+                    <div className='row skills-box'>
+                        {
+                            value_other.map((value_other,index) => {
+                                return <>   
+                                    <div className='col-lg-4 col-mg-12 col-sm-12 my-1'>
+                                        <span className='language'>{value_other.text}</span>
+                                        <div className='progress-some'>
+                                            <div className='progress-new' style={{width: `${value_other.percentage}%`}}></div>
+                                        </div>
+                                    </div>
+                                </>
+                            })
+                        } 
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+    </>
 }
 
 
