1979e01fa59df4b7d6c5c641b43eeef54657d9be
Marinasy Behamby <behambymarinasy@gmail.com>
2022-12-02 10:14:10 +0300

    add language menu

--- src/personalisation/components/Resume.jsx
+++ src/personalisation/components/Resume.jsx
@@ -2,7 +2,7 @@ import Header  from './SectionHeader';
 import '../styles/Resume.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUserGraduate, faBriefcase, faCode, faListCheck} from '@fortawesome/free-solid-svg-icons'
+import { faUserGraduate, faBriefcase, faCode, faListCheck, faLanguage} from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react';
 import Skills  from './Skills';
 import Projects from './Projects';
@@ -18,7 +18,8 @@ const userGraduateIcon = <FontAwesomeIcon icon={faUserGraduate}/>;
 const workIcon =  <FontAwesomeIcon icon={faBriefcase} /> 
 const codeIcon = <FontAwesomeIcon icon={faCode} />
 const projectsListIcon = <FontAwesomeIcon icon={faListCheck} /> 
-
+const languageIcon = <FontAwesomeIcon icon={faLanguage} />
+ 
 const title = {
     title: 'Resume',
     desc: 'my resume'
@@ -30,13 +31,15 @@ const Resume = () =>
     const [skills, setSkills] = useState(false); 
     const [work, setWork] = useState(false); 
     const [projects, setProjects] = useState(false);
+    const [languages, setLanguages] = useState(false);
 
 
     const setAllFalse = () => {
         setEducation(false);        
         setWork(false);        
         setProjects(false);        
-        setSkills(false);             
+        setSkills(false);   
+        setLanguages(false);          
     }
 
     const changeState = (function1, state1) => {
@@ -56,12 +59,15 @@ const Resume = () =>
                         <span className='icons-span'>{workIcon}</span>
                         <span className='icons-span'>{codeIcon}</span>
                         <span className='icons-span'>{projectsListIcon}</span>
+                        <span className='icons-span'>{languageIcon}</span>
                     </div>
                     <div className='d-flex flex-column'>
                         <span className={ education === false ? 'resume-option-items' : 'selected resume-option-items'} onClick={ () => { changeState(setEducation,true); }}>Education</span>                        
                         <span className={ work === false ? 'resume-option-items' : 'selected resume-option-items'} onClick={ () => { changeState(setWork,true); }}>Work</span>
+                        <span className={ skills === false ? 'resume-languagestems' : 'selected resume-option-items'} onClick={ () => { changeState(setSkills,true); }}>Skills</span>
                         <span className={ skills === false ? 'resume-option-items' : 'selected resume-option-items'} onClick={ () => { changeState(setSkills,true); }}>Skills</span>
                         <span className={ projects === false ? 'resume-option-items' : 'selected resume-option-items'} onClick={ () => { changeState(setProjects,true); }}>Projects</span>
+                        <span className={ languages === false ? 'resume-option-items' : 'selected resume-option-items'} onClick={ () => { changeState(setLanguages,true); }}>Languages</span>
                     </div>
                 </div>
                 <div className='col-lg-8 col-md-8 resume-right-section'>
